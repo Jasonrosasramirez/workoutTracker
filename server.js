398b9527a8263e352cbf214a4c80dff7946b68ab
Jason <jrosasrjf@gmail.com>
2021-06-22 22:21:16 -0500

    populated server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 /* -- Basic imports -- */
 const express = require("express"); 
 const router = express.Router(); 
-
 const mongoose = require("mongoose");
 
+const app = express(); 
 
 
-/* -- Listing on Port 3000 -- */
+/* -- utilizing the app -- */
+
+
+
+/* -- connection with mongo atlas -- */
+mongoose.connect(); 
+
+
+
+
+/* -- Listing on Port 4000 -- */
+const PORT = process.env.PORT || 4000; 
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`); // displaying the variable port information. 
 });
