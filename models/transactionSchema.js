b63a1d4ce94a4e08f8c1cea20f6a7cfbd777a2a3
Jason <jrosasrjf@gmail.com>
2021-06-22 21:49:49 -0500

    started populating the transaction schema

---
+++ models/transactionSchema.js
@@ -0,0 +1,15 @@
+/* -- Requiring variables -- */
+const express = require("express"); 
+const router = express.Router(); 
+
+
+/* -- Building the new Schema -- */
+const Schema; 
+
+
+
+
+
+
+/* -- exporting -- */
+module.export = router;
