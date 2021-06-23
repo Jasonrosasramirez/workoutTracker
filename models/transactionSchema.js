17244fe700daafe1dbc9c5dd5644c4c8bc20b7d6
Jason <jrosasrjf@gmail.com>
2021-06-22 22:09:13 -0500

    added in basic schema for the transaction

--- models/transactionSchema.js
+++ models/transactionSchema.js
@@ -5,10 +5,29 @@ const Schema = mongoose.Schema;
 
 /* -- Building the new Schema -- */
 const schemaTemplate = new Schema({
+    // schemas are built as objects 
 
+        description: {
+            type: String,
+            trim: true, 
+            required: "Hello There! What is the transaction for? " 
+        }, 
 
-    }
-  );
+        action: {
+            type: Boolean, 
+            required: "Is this a withdrawal? "
+        },
+
+        amount: {
+            type: Number, 
+            required: "How much will it be today? " 
+        },
+
+        date: {
+            type: Date, 
+            default: Date.now
+        }
+    });
 
 
 
