d57350429c7e7f8e6d5532496b3c4cc98c3df48b
Jason <jrosasrjf@gmail.com>
2021-07-15 23:45:33 -0500

    added in code to server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 /* -- Basic imports -- */
 const express = require("express"); 
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression"); 
 
+const PORT = process.env.PORT || 4000; 
 const app = express(); 
-
 require("dotenv").config(); // references the .env file
+
 
 /* -- utilizing the app -- */
 app.use(logger("dev"));
@@ -17,14 +20,17 @@ app.use(express.static("public"));
 
 
 /* -- connection with mongo atlas -- */
-mongoose.connect(); 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nudget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+}); 
 
 
+/* -- routes -- */
+app.use(require("./routes/api.js"));
 
 
 /* -- Listing on Port 4000 -- */
-const PORT = process.env.PORT || 4000; 
-
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`); // displaying the variable port information. 
 });
